d13705b30ea3986bfc29a9ad60a5701001c36cd0
Steve Godin <steve.god@gmail.com>
2015-11-21 14:01:27 -0800

    Add meme support

--- app/main/main.js
+++ app/main/main.js
@@ -4,10 +4,12 @@ const Path          = require("path");
 const App           = require("app");
 const BrowserWindow = require("browser-window");
 
-// Setting Paths
+// Set Paths
 const appRoot = Path.resolve(__dirname, "../..");
 global.pathTo = {
   images:   Path.resolve(appRoot, "app/assets/images"),
+  cache:    Path.resolve(appRoot, "cache"),
+  lib:      Path.resolve(appRoot, "lib"),
   renderer: Path.resolve(appRoot, "app/renderer"),
   root:     Path.resolve(appRoot)
 }
@@ -16,8 +18,10 @@ App.on("ready", function () {
   // Once app is loaded & ready, create a mainWindow.
   // BrowserWindow loads it in a new renderer process.
   const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 500,
     frame: true,
-    resizable: true,
+    resizable: false
   });
 
   mainWindow.loadURL(`file://${pathTo.renderer}/main/index.html`);
---
+++ app/renderer/main/index.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const Remote = require("remote");
+const pathTo = Remote.getGlobal("pathTo");
+
+const Meme    = require(`${pathTo.lib}/meme`);
+const Q       = require("q");
+const Request = require("request");
+const fs      = require("fs");
+const lwip    = require("lwip");
+
+const searchQueryInput = document.querySelector("#search-query");
+const adviceAnimalImg = document.querySelector("#advice-animal");
+
+searchQueryInput.addEventListener("input", function (event) {
+  global.adviceAnimalQuestion = event.target.value;
+});
+
+searchQueryInput.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    loading();
+    const promise = Meme.say(global.adviceAnimalQuestion);
+    promise.delay(200).then(downloadImage).then(setImage).done();
+  }
+});
+
+function getResponse (url) {
+  return Q.Promise(function (resolve, reject, notify) {
+
+    console.log("Getting response...");
+    Request(url, function (error, response) {
+      if (error) {
+        reject(new Error(error));
+      } else {
+        resolve(Q.resolve(response).delay(200).then(downloadImage));
+      }
+    });
+
+  })
+};
+
+function downloadImage (response) {
+  return Q.Promise(function (resolve, reject, notify) {
+
+    const url = response.request.href;
+
+    if (response.statusCode === 404) {
+      console.log("404 – Not Found; Trying again...");
+      resolve(Q.resolve(url).then(getResponse));
+    } else {
+
+      const extension   = /image\/(\w+)\b/.exec(response.headers["content-type"])[1];
+      const filename    = response.headers["meme-text"].replace(/[^A-Za-z+]/g, "").replace(/\+/g,"-");
+      const location    = `${pathTo.cache}/${filename}.${extension}`;
+      const writeStream = fs.createWriteStream(location);
+
+      console.log(`Downloading ${url}...`);
+      Request(url).pipe(writeStream);
+
+      writeStream.on("finish", function () {
+        this.originResponse = response;
+        resolve(verifyImage(this));
+      });
+
+      writeStream.on("error", function (error) { reject(new Error(error)) });
+    }
+  })
+};
+
+function verifyImage (writeStream) {
+  return Q.Promise(function (resolve, reject, notify) {
+
+    console.log("Verifying image...");
+    lwip.open(writeStream.path, function (error, image) {
+      if (error) {
+        console.log(error);
+        resolve(Q.resolve(writeStream.originResponse).delay(200).then(downloadImage));
+      } else {
+        resolve(writeStream.path);
+      }
+    });
+
+  })
+};
+
+function setImage (path) {
+  console.log("Setting image...");
+  adviceAnimalImg.src = path;
+  adviceAnimalImg.className = "";
+}
+
+function loading () {
+  adviceAnimalImg.src = "../../assets/images/doge-icon-512.png";
+  adviceAnimalImg.className = "loading";
+}
