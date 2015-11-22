9e92cb63508379c09f89e8b2f97cd6827e08a3df
Steve Godin <steve.god@gmail.com>
2015-11-21 20:27:42 -0800

    Add tray icon with menu

--- app/main/main.js
+++ app/main/main.js
@@ -1,8 +1,10 @@
 "use strict";
 
-const Path          = require("path");
 const App           = require("app");
 const BrowserWindow = require("browser-window");
+const Path          = require("path");
+const Tray          = require("tray");
+const Menu          = require("menu");
 
 // Set Paths
 const appRoot = Path.resolve(__dirname, "../..");
@@ -13,16 +15,32 @@ global.pathTo = {
   renderer: Path.resolve(appRoot, "app/renderer"),
   root:     Path.resolve(appRoot)
 }
+const trayIconPath = `${pathTo.images}/tray.png`;
 
 App.on("ready", function () {
-  // Once app is loaded & ready, create a mainWindow.
-  // BrowserWindow loads it in a new renderer process.
   const mainWindow = new BrowserWindow({
     width: 400,
     height: 500,
     frame: true,
     resizable: false
   });
-
   mainWindow.loadURL(`file://${pathTo.renderer}/main/index.html`);
+
+  const menuConfig = [ {
+    label: "Toggle DevTools",
+    accelerator: "Alt+Command+I",
+    click: function() {
+      mainWindow.show();
+      mainWindow.toggleDevTools();
+    }
+  }, {
+    label: "Quit",
+    accelerator: "Command+Q",
+    selector: "terminate:"
+  } ];
+  const contextMenu = Menu.buildFromTemplate(menuConfig);
+
+  const trayIcon = new Tray(trayIconPath);
+  trayIcon.setToolTip("Animal Advisor");
+  trayIcon.setContextMenu(contextMenu);
 });
