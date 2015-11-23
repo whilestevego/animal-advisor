2b344b256f6b48cc95984807a777acee053d68d1
Steve Godin <steve.god@gmail.com>
2015-11-22 18:44:07 -0800

    Add context meny save image as... item

--- app/renderer/main/index.js
+++ app/renderer/main/index.js
@@ -1,12 +1,21 @@
 "use strict";
 
-const Remote   = require("remote");
-const pathTo   = Remote.getGlobal("pathTo");
-const Menu     = Remote.require('menu');
-const MenuItem = Remote.require('menu-item');
+// External Modules
+const Request = require("request");
 
+// Node Modules
+const FS      = require("fs");
+const Path    = require("path");
+
+// Electron Modules
+const Shell    = require("shell");
+const Remote   = require("remote");
+const Menu     = Remote.require("menu");
+const MenuItem = Remote.require("menu-item");
+const Dialog   = Remote.require("dialog");
+
+const pathTo   = Remote.getGlobal("pathTo");
 const AnimalAdvisor = require(`${pathTo.lib}/animal-advisor`);
-const Request       = require("request");
 
 const searchQueryInput = document.querySelector("#search-query");
 const adviceAnimalImg  = document.querySelector("#advice-animal");
@@ -52,19 +61,59 @@ function sendNotification (path) {
 const adviceAnimalMenu = new Menu();
 
 adviceAnimalMenu.append(new MenuItem({
-  label: 'Reset',
+  label: "Reset",
   click: resetAdviceAnimal
 }));
 adviceAnimalMenu.append(new MenuItem({
-  label: 'MenuItem1',
-  click: function(menuItem) { console.log(menuItem); console.log('item 1 clicked'); }
-}));
-adviceAnimalMenu.append(new MenuItem({
-  label: 'MenuItem2',
-  type: 'checkbox', checked: true
+  label: "Save Image as...",
+  click: showSaveImageAsDialog,
+  accelerator: "Command+Shift+S"
 }));
 
-adviceAnimalImg.addEventListener('contextmenu', function (event) {
+adviceAnimalImg.addEventListener("contextmenu", function (event) {
   event.preventDefault();
   adviceAnimalMenu.popup(Remote.getCurrentWindow());
 }, false);
+
+function showSaveImageAsDialog() {
+  const options = { title: "Save Image as..." };
+  Dialog.showSaveDialog(Remote.getCurrentWindow(), options, handleSaveDialog);
+}
+
+function handleSaveDialog (path) {
+  const sourcePath = event.target.src.replace(/file:\/\//,"");
+  const extension = Path.extname(sourcePath);
+  const destinationPath = path + extension;
+
+  copyFile(sourcePath, destinationPath, handleCopy(destinationPath));
+}
+
+function handleCopy (path) {
+  return function (error) {
+    if (error) {
+      Dialog.showErrorBox("Save Image as... failed", error)
+    } else if (path) {
+      Shell.showItemInFolder(path);
+    };
+  }
+};
+
+function copyFile(source, target, callback) {
+  let callbackCalled = false;
+
+  const readStream = FS.createReadStream(source);
+  readStream.on("error", function(error) { done(error); });
+
+  const writeStream = FS.createWriteStream(target);
+  writeStream.on("error", function(error) { done(error); });
+  writeStream.on("close", function(ex) { done(); });
+
+  readStream.pipe(writeStream);
+
+  function done(error) {
+    if (!callbackCalled && callback) {
+      callback(error);
+      callbackCalled = true;
+    }
+  }
+}
