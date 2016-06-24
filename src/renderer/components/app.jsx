cd8040ed3eef9ef091bbfa4086e375846a9fab96
Steve Godin <steve.god@gmail.com>
2016-06-23 23:35:00 -0700

    Remove unecessary app nesting div

--- src/renderer/components/app.jsx
+++ src/renderer/components/app.jsx
@@ -2,9 +2,5 @@ import React from 'react'
 import Consultation from './consultation.js'
 
 export default function App (props) {
-  return (
-    <section className='app'>
-      <Consultation/>
-    </section>
-  )
+  return <Consultation/>
 }
